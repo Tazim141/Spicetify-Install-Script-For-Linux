7ddc47706ad8f12423f38e46357f478f8d907f2e
Tazim <59084210+Tazim141@users.noreply.github.com>
2024-03-28 20:15:28 +0600

    Fix gpg key error

--- setup_spotify_spicetify.sh
+++ setup_spotify_spicetify.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 # Add Spotify repository key
-curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo apt-key add -
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 
 # Add Spotify repository to sources list
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
-# Update package lists and install Spotify
-sudo apt-get update && sudo apt-get install -y spotify-client
+# Update package lists
+sudo apt-get update
 
-# Set permissions for Spotify directory
+# Install Spotify client
+sudo apt-get install spotify-client -y
+
+# Set permissions for Spotify directory and its contents
 sudo chmod a+wr /usr/share/spotify
 sudo chmod a+wr /usr/share/spotify/Apps -R
 
-# Install spicetify-cli
+# Install Spicetify CLI
 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
+
+
+
 
 
